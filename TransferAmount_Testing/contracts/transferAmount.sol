7420372c9670e647ecf76b71dc3d05c5d024dbdc
Sandeep Chowdhury <49147378+sandeepiitism@users.noreply.github.com>
2022-07-22 13:53:41 +0530

    Add files via upload

---
+++ TransferAmount_Testing/contracts/transferAmount.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+contract TransferAmount {
+    address owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function send(address payable[] memory to, uint256[] memory amount) public payable ownerOnly {
+        require(to.length == amount.length, " to must be same length as amount");
+        for (uint256 i =0; i< to.length; i++) {
+            to[i].transfer(amount[i]);
+        }
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+}
